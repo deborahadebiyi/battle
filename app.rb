9773804cc367bbbe2721bdf5d6db897b0abce849
deborah adebiyi <deborahadebiyi@hotmail.co.uk>
2020-10-28 16:35:44 +0000

    Redirect get pattern

--- app.rb
+++ app.rb
@@ -1,16 +1,23 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
+  enable :sessions
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_name_one = params[:player_name_one]
-    @player_name_two = params[:player_name_two]
-    erb :play
+    session[:player_name_one] = params[:player_name_one]
+    session[:player_name_two] = params[:player_name_two]
+    redirect '/play'
   end
 
+  get '/play' do
+    @player_name_one = session[:player_name_one]
+    @player_name_two = session[:player_name_two]
+    erb :play
+  end
 
 
   run! if app_file == $0
