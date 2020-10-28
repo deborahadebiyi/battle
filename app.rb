a790d23b3a71b12659f7b857eb76c2ce209dd175
deborah adebiyi <deborahadebiyi@hotmail.co.uk>
2020-10-28 16:15:10 +0000

    Entering players

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working'
+    erb :index
+  end
+
+  post '/names' do
+    @player_name_one = params[:player_name_one]
+    @player_name_two = params[:player_name_two]
+    erb :play
   end
 
 
--- spec/features/testing_infrastructure_working_spec.rb
+++ spec/features/testing_infrastructure_working_spec.rb
@@ -6,3 +6,13 @@ feature 'Testing infrastructure working' do
     expect(page).to have_content 'Testing infrastructure working'
   end
 end
+
+feature 'Display player names' do
+  scenario 'gets user input and exposes their names on screen' do
+    visit('/')
+    fill_in :player_name_one, with: 'Ed'
+    fill_in :player_name_two, with: 'Eddie'
+    click_button 'Submit'
+    expect(page).to have_content 'Ed and Eddie'
+  end
+end
